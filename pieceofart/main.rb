3afb5fde9623110b147c8f2c3951ded7f35bd7b5
Magdalena Bialon <magdalenabialon@gmail.com>
2016-08-01 23:07:33 +1000

    created session, login/logout methods, 2users with passwords, model

--- pieceofart/main.rb
+++ pieceofart/main.rb
@@ -12,6 +12,21 @@ require_relative 'models/user'
 
 enable :sessions
 
+helpers do
+  def logged_in?
+    if User.find_by(id: session[:user_id])
+      return true
+    else
+      return false
+    end
+  end
+end
+
+
+def current_user
+  User.find(session[:user_id])
+end
+
 
 
 
@@ -21,5 +36,64 @@ end
 
 
 post '/session' do
-  "Hello World"
+  user = User.find_by(email: params[:email])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect to '/'
+  else
+    erb :index
+  end
 end
+
+
+delete '/session' do
+    session[:user_id] = nil
+    redirect to '/'
+end
+
+
+
+get '/my_album' do
+  erb :my_album
+end
+
+
+get '/results' do
+  "Hello World"
+  # sql = "SELECT * FROM pokemon WHERE species = '#{params["species"]}';"
+  # @pokem = run_sql(sql)[0]
+  # erb :results
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# [2] pry(main)> u1=User.new  => #<User:0x007fb1ab5c0c10 id: nil, name: nil, email: nil, password_digest: nil>
+# [3] pry(main)> u1.name = 'magda'  => "magda"
+# [4] pry(main)> u1.email = 'mb@g.co'  => "mb@g.co"
+# [5] pry(main)> u1.password = 'cake'  => "cake"
+
+# [8] pry(main)> u2 = User.new  => #<User:0x007fb1ab713a40 id: nil, name: nil, email: nil, password_digest: nil>
+# [9] pry(main)> u2.name = 'dt'   => "dt"
+# [10] pry(main)> u2.email = 'dt@ga.co'  => "dt@ga.co"
+# [11] pry(main)> u2.password = 'pudding'  => "pudding"
--- pieceofart/models/user.rb
+++ pieceofart/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
 
   validates :name, presence:true, allow_blank: false
-  
+
   has_secure_password
+  
   has_many :paintings
   has_many :comments
 
